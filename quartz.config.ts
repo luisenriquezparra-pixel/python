097e90cb35accaf3635ad34c7735bc3bbd9998bc
luis <luisenriquezparra@gmail.com>
2026-05-05 19:08:36 -0400

    cambios

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "luisenriquezparra-pixel.github.io/python",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
